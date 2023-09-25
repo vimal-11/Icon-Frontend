801173502ba121a4d86f73394e92249db0f5ce88
root <root@vps.in>
2023-09-25 19:47:34 +0200

    assc tres added

--- components/Aboutus.jsx
+++ components/Aboutus.jsx
@@ -50,6 +50,17 @@ const contributors = [
       "It is a long-established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
   },
   {
+    name: 'Bharani M',
+    desc: 'Assosiate Treasurer',
+    image: 'bharani.jpg',
+    link1: '#',
+    link2: 'https://www.linkedin.com/in/bharani-m-b32265287',
+    link3: 'https://www.instagram.com/bharani_kcm/',
+    courseandyear: 'CSE, III Year',
+    description:
+      "It is a long-established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+  },
+  {
     name: 'Robin Brito',
     desc: 'M.Tech Representative',
     image: 'robin.jpg',
